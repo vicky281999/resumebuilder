906a10ee5645a408350361b17a913208dd87dfea
ZOHOCORP\rathi-pt1424 <rathishr10@gmail.com>
2018-08-08 16:01:37 +0530

    testing

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <html>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <div class='fullscreenDiv'>
     <div class="center" style="position: absolute;
                                color: blueviolet;
@@ -10,4 +11,15 @@
     margin-left: -50px;
     margin-top: -25px; ">Comming Soon.....</div>
 </div>
+    <script>
+$.ajax({
+  url: "https://api.github.com/users/rathishsekar/repos",
+  jsonp: true,
+  method: "GET",
+  dataType: "json",
+  success: function(res) {
+    console.log(res)
+  }
+});
+        </script>
 </html>
